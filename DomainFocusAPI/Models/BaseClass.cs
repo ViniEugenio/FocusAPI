8f860eeb49094265b0dda6bf1850322503e02f54
Vinícius Fernandes Eugênio <vinicius.eugenio@am4.com.br>
2020-06-20 01:07:10 -0300

    Fist Commit

---
+++ DomainFocusAPI/Models/BaseClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainFocusAPI.Models
+{
+    public abstract class BaseClass
+    {
+        public Guid Id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        public bool Ativo { get; set; }         
+    }
+}
---
+++ DomainFocusAPI/Models/Tarefa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainFocusAPI.Models
+{
+    public class Tarefa : BaseClass
+    {
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public DateTime HoraInicio { get; private set; }
+        public DateTime HoraTermino { get; private set; }
+    }
+}
