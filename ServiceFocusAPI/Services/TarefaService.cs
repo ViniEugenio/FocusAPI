2922d333d79cf9a915cbf1fc40c64ddbbdb211a6
Vinícius Fernandes Eugênio <vinicius.eugenio@am4.com.br>
2020-06-20 02:30:46 -0300

    Services

---
+++ ServiceFocusAPI/Services/TarefaService.cs
@@ -0,0 +1,24 @@
+﻿using DomainFocusAPI.DTO;
+using DomainFocusAPI.Interfaces;
+using DomainFocusAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceFocusAPI.Services
+{
+    public class TarefaService
+    {
+        private readonly ITarefaRepositorie _Repositorie;
+
+        public TarefaService(ITarefaRepositorie repositorie)
+        {
+            _Repositorie = repositorie;
+        }
+
+        public void CadastrarTarefa(Tarefa model)
+        {
+            _Repositorie.Adicionar(model);
+        }
+    }
+}
