b32df93b77d1f382536252f67d435668d38488c6
Vinícius Fernandes Eugênio <vinicius.eugenio@am4.com.br>
2020-06-20 02:09:08 -0300

    .

--- DomainFocusAPI/Models/User.cs
+++ DomainFocusAPI/Models/User.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DomainFocusAPI.Models
 {
-    class User : IdentityUser
+    public class User : IdentityUser
     {
         
     }
